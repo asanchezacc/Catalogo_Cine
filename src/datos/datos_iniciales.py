4439fb303f649932ccb24ee8e11869ef587753fd
asanchezacc <antonio.sa.09@hotmail.com>
2026-08-31 20:32:25 -0300

    Implementa CRUD de peliculas y datos iniciales

---
+++ src/datos/datos_iniciales.py
@@ -0,0 +1,18 @@
+"""
+Datos iniciales del sistema de cine
+"""
+
+# Catálogo de películas inicial
+peliculas_iniciales = [
+    {"id": 1, "titulo": "Spiderman Brand New Day", "genero": "Acción", "duracion": 150, "precio": 250},
+    {"id": 2, "titulo": "La Odisea", "genero": "Drama", "duracion": 90, "precio": 200},
+    {"id": 3, "titulo": "Supergirl", "genero": "Ciencia ficción", "duracion": 120, "precio": 220},
+    {"id": 4, "titulo": "Mortal Kombat II", "genero": "Acción", "duracion": 100, "precio": 240},
+    {"id": 5, "titulo": "Maestros del Universo", "genero": "Aventura", "duracion": 110, "precio": 230}
+]
+
+# Usuarios de prueba (admin y empleado)
+usuarios_iniciales = [
+    {"usuario": "admin", "contraseña": "admin123", "rol": "admin"},
+    {"usuario": "empleado", "contraseña": "emp123", "rol": "empleado"},
+]
